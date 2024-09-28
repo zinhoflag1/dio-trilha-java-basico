d63837ba1f0de7ab782041d2edc6c41a045bab72
zinhoflag1 <zinhoflag1@gmail.com>
2024-09-28 00:56:55 -0300

    salvar final

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor digite o numero da sua Conta :");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor digite o numero da sua Agencia :");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor digite o seu Nome :");
+        String nome = scanner.next();
+
+        double saldo = 237.48;
+
+
+
+        System.out.println("Olá ["+ nome + "] obrigado por criar uma conta em nosso banco, sua agência é : ["+ agencia + "], conta ["+ conta +"] e seu saldo ["+ saldo +"] já está disponível para saque");
+
     }
 }
